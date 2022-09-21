9048ec64da5ba06d72081073788aa8788bacfd1d
lib-yoshida <k_yoshida@libcon.co.jp>
2022-09-21 13:30:31 +0900

    pc size create

---
+++ index.js
@@ -0,0 +1,55 @@
+const onClickRow = (number) => {
+  const reset = () => {
+    document.querySelector("#row1").classList.add("hidden");
+    document.querySelector("#row2").classList.add("hidden");
+    document.querySelector("#row3").classList.add("hidden");
+    document.querySelector("#r-content1").classList.add("hidden");
+    document.querySelector("#r-content2").classList.add("hidden");
+    document.querySelector("#r-content3").classList.add("hidden");
+    document.querySelector("#wrapper").classList.add("closeRadius");
+    document.querySelector("#wrapper").classList.remove("openRadius");
+  };
+  switch (number) {
+    case 1:
+      if (
+        document.querySelector("#row1").classList[1] === "hidden" &&
+        document.querySelector("#r-content1").classList[1] === "hidden"
+      ) {
+        reset();
+        document.querySelector("#row1").classList.remove("hidden");
+        document.querySelector("#r-content1").classList.remove("hidden");
+        document.querySelector("#wrapper").classList.remove("closeRadius");
+        return;
+      }
+      reset();
+      return;
+    case 2:
+      if (
+        document.querySelector("#row2").classList[1] === "hidden" &&
+        document.querySelector("#r-content2").classList[1] === "hidden"
+      ) {
+        reset();
+        document.querySelector("#row2").classList.remove("hidden");
+        document.querySelector("#r-content2").classList.remove("hidden");
+        document.querySelector("#wrapper").classList.remove("closeRadius");
+        return;
+      }
+      reset();
+      return;
+    case 3:
+      if (
+        document.querySelector("#row3").classList[1] === "hidden" &&
+        document.querySelector("#r-content3").classList[1] === "hidden"
+      ) {
+        reset();
+        document.querySelector("#row3").classList.remove("hidden");
+        document.querySelector("#r-content3").classList.remove("hidden");
+        document.querySelector("#wrapper").classList.remove("closeRadius");
+        return;
+      }
+      reset();
+      return;
+    default:
+      return;
+  }
+};
